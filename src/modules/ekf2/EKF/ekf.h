b949c30a291470f083def4190708379e65bbcbeb
Roman Bapst <bapstroman@gmail.com>
2024-01-11 13:09:22 +0100

    added support to reset vehicle position based on external position
    via command MAV_CMD_EXTERNAL_POSITION_ESTIMATE
    
    backport of 603c3f6 by RomanBapst <bapstroman@gmail.com>

--- src/modules/ekf2/EKF/ekf.h
+++ src/modules/ekf2/EKF/ekf.h
@@ -336,6 +336,9 @@ public:
 
 	const BaroBiasEstimator::status &getBaroBiasEstimatorStatus() const { return _baro_b_est.getStatus(); }
 
+	void resetGlobalPosToExternalObservation(double lat_deg, double lon_deg, float accuracy,
+			uint64_t timestamp_observation);
+
 private:
 
 	// set the internal states and status to their default value
@@ -654,7 +657,9 @@ private:
 	void resetHorizontalPositionToVision();
 	void resetHorizontalPositionToOpticalFlow();
 	void resetHorizontalPositionToLastKnown();
-	void resetHorizontalPositionTo(const Vector2f &new_horz_pos);
+	void resetHorizontalPositionToExternal(const Vector2f &new_horiz_pos, float horiz_accuracy);
+	void resetHorizontalPositionTo(const Vector2f &new_horz_pos, const Vector2f &new_horz_pos_var);
+	void resetHorizontalPositionTo(const Vector2f &new_horz_pos, const float pos_var = NAN) { resetHorizontalPositionTo(new_horz_pos, Vector2f(pos_var, pos_var)); }
 
 	void resetVerticalPositionTo(float new_vert_pos);
 
