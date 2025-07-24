2e7e0018f0186669e1c3a21a99c0eba572be3d2a
Torjus Bakkene <torjus.bakkene@gmail.com>
2025-07-24 13:19:10 +0200

    ms4525do: log corupted samples
    For debugging airspeed sensor errors.
    
    This logs the number of corrupted samples as a perf counter, as well as
    the number of samples with corrupted LSBs which have been accepted.

--- src/drivers/differential_pressure/ms4525do/MS4525DO.hpp
+++ src/drivers/differential_pressure/ms4525do/MS4525DO.hpp
@@ -93,5 +93,7 @@ private:
 
 	perf_counter_t _sample_perf{perf_alloc(PC_ELAPSED, MODULE_NAME": read")};
 	perf_counter_t _comms_errors{perf_alloc(PC_COUNT, MODULE_NAME": communication errors")};
-	perf_counter_t _fault_perf{perf_alloc(PC_ELAPSED, MODULE_NAME": fault detected")};
+	perf_counter_t _fault_perf{perf_alloc(PC_COUNT, MODULE_NAME": fault detected")};
+	perf_counter_t _corrupted_samples_perf{perf_alloc(PC_COUNT, MODULE_NAME": corrupted samples discarded")};
+	perf_counter_t _corrupted_accepted_lsb_samples_perf{perf_alloc(PC_COUNT, MODULE_NAME": samples accpted with corrupted LSBs")};
 };
