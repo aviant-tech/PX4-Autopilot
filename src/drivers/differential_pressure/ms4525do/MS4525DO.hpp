11570fe2ca474deff324bd004844ef55e98dfc69
Torjus Bakkene <torjus.bakkene@gmail.com>
2025-07-24 15:25:17 +0200

    ms4525do: fix type of _fault_perf
    It should be a counter, not elapsed

--- src/drivers/differential_pressure/ms4525do/MS4525DO.hpp
+++ src/drivers/differential_pressure/ms4525do/MS4525DO.hpp
@@ -93,5 +93,5 @@ private:
 
 	perf_counter_t _sample_perf{perf_alloc(PC_ELAPSED, MODULE_NAME": read")};
 	perf_counter_t _comms_errors{perf_alloc(PC_COUNT, MODULE_NAME": communication errors")};
-	perf_counter_t _fault_perf{perf_alloc(PC_ELAPSED, MODULE_NAME": fault detected")};
+	perf_counter_t _fault_perf{perf_alloc(PC_COUNT, MODULE_NAME": fault detected")};
 };
