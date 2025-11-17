2e6c2776dd0e03c5901dceb29d06ed3eab4d1c6c
oystub <o_github@oystub.com>
2025-11-17 14:10:26 +0100

    mavlink: use aviant dialect

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1443,6 +1443,9 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("VFR_HUD", 4.0f);
 		configure_stream_local("VIBRATION", 0.1f);
 		configure_stream_local("WIND_COV", 0.5f);
+#if defined(MAVLINK_ENABLED_AVIANT)
+		configure_stream_local("AVIANT_HEARTBEAT", 1.0f);
+#endif // MAVLINK_ENABLED_AVIANT
 
 #if !defined(CONSTRAINED_FLASH)
 		configure_stream_local("DEBUG", 1.0f);
@@ -1682,6 +1685,10 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("VFR_HUD", 20.0f);
 		configure_stream_local("VIBRATION", 2.5f);
 		configure_stream_local("WIND_COV", 10.0f);
+
+#if defined(MAVLINK_ENABLED_AVIANT)
+		configure_stream_local("AVIANT_HEARTBEAT", 1.0f);
+#endif // MAVLINK_ENABLED_AVIANT
 
 #if !defined(CONSTRAINED_FLASH)
 		configure_stream_local("DEBUG", 50.0f);
