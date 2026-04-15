2019ec80da4124db242e4eb4de47cb69af088530
Igor Mišić <igy1000mb@gmail.com>
2026-04-15 15:08:24 +0200

    fix(navigator): update mission index when item is NAV_CMD_DO_JUMP

--- src/modules/navigator/mission.cpp
+++ src/modules/navigator/mission.cpp
@@ -186,6 +186,11 @@ Mission::do_need_move_to_takeoff()
 
 void Mission::setActiveMissionItems()
 {
+	if (_mission_item.nav_cmd == NAV_CMD_DO_JUMP) {
+		set_current_mission_index(_mission_item.do_jump_mission_index);
+		return;
+	}
+
 	/* Get mission item that comes after current if available */
 	static constexpr size_t max_num_next_items{2u};
 	int32_t next_mission_items_index[max_num_next_items];
