502bf1c6fd35983a10006d259158784356ac9289
Igor <igy1000mb@gmail.com>
2026-09-16 06:44:21 +0000

    aviant/navigation: avoid float formatting in mag heading messages

--- src/modules/aviant/navigation/Navigation.cpp
+++ src/modules/aviant/navigation/Navigation.cpp
@@ -265,10 +265,12 @@ void Navigation::checkMagHeadingOffset(aviant_navigation_s *out)
 		return;
 	}
 
+	const int offset_rounded = (int)roundf(offset_deg);
+
 	if ((offset_deg > MAG_HDG_FAIL_DEG) && !_mag_hdg_fail_msg_sent) {
 		_mag_hdg_fail_msg_sent = true;
-		mavlink_log_critical(&_mavlink_log_pub, "Mag heading offset %.0f deg, limit %.0f deg\t",
-				     (double)offset_deg, (double)MAG_HDG_FAIL_DEG);
+		mavlink_log_critical(&_mavlink_log_pub, "Mag heading offset %d deg, limit %d deg\t",
+				     offset_rounded, (int)MAG_HDG_FAIL_DEG);
 		events::send<float, float>(events::ID("aviant_nav_mag_hdg_offset_fail"),
 		{events::Log::Critical, events::LogInternal::Critical},
 		"Mag heading offset {1:.0} deg, limit {2:.0} deg",
@@ -276,8 +278,8 @@ void Navigation::checkMagHeadingOffset(aviant_navigation_s *out)
 
 	} else if ((offset_deg > MAG_HDG_WARN_DEG) && !_mag_hdg_warn_msg_sent) {
 		_mag_hdg_warn_msg_sent = true;
-		mavlink_log_warning(&_mavlink_log_pub, "Mag heading offset %.0f deg, expected below %.0f deg\t",
-				    (double)offset_deg, (double)MAG_HDG_WARN_DEG);
+		mavlink_log_warning(&_mavlink_log_pub, "Mag heading offset %d deg, expected below %d deg\t",
+				    offset_rounded, (int)MAG_HDG_WARN_DEG);
 		events::send<float, float>(events::ID("aviant_nav_mag_hdg_offset_warn"),
 		{events::Log::Warning, events::LogInternal::Warning},
 		"Mag heading offset {1:.0} deg, expected below {2:.0} deg",
